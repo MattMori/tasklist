f0b63852e981eb01dae4479bb57b859f36b6d6bc
MattMori <Mateus5310@live.com>
2023-07-31 19:13:13 -0300

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+const Header = () => {
+    return (
+        <section className="Header">
+            <ul className="Header__menu-itens">
+                <li><a href="">Organização</a></li>
+                <li className="Header__menu-itens__focus"><a href="">Tarefas</a></li>
+                
+            </ul>
+        </section>
+    )
+};
+
+export default Header;
